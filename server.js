3d77ac61a516271f3497e95925c589ed71635bc6
vajrasami <vajrsami@gmail.com>
2023-12-05 18:53:59 +0545

    comments added line by line

--- server.js
+++ server.js
@@ -1,52 +1,39 @@
-//importing modules
-const express = require ("express");
-const dotenv = require ("dotenv").config();
-const jwt = require ("jsonwebtoken");
+// Importing required modules
+const express = require("express"); // Express.js for building the web server
+const dotenv = require("dotenv").config(); // Loading environment variables from a .env file
+const jwt = require("jsonwebtoken"); // JSON Web Token handling
 
-//initialize 
-const app = express ();
+// Initialize Express app and set a secret key for JWT
+const app = express();
 const secretKey = "secretKey";
 
-//middleware for fetching data
-// app.get("/", (req,res) =>
-// {
-//     res.json("Hello! It's running.");
-// });
-
-//port define
+// Define the port for the server to listen on
 const PORT = process.env.PORT || 5000;
 
-
-//for authentication 
-app.post("/login", (req,res)=>
-{
-    //controller
+// Authentication route for generating JWT token upon login
+app.post("/login", (req, res) => {
+    // Controller - Simulated user data
     const user = {
         id: 1,
         name: "user",
         email: "user@gmail.com",
     };
 
-
-    //creates a jwt token
-    jwt.sign({user}, secretKey, {expiresIn: "50s"}, (err,token)=>{
+    // Generate JWT token with a 50-second expiration
+    jwt.sign({ user }, secretKey, { expiresIn: "50s" }, (err, token) => {
         res.json({
             token,
         });
     });
 });
 
-
-//Profile Route (/profile) with Token Verification Middleware
-//protected by the verifyToken that only displays data eith message if the token is Valid
-app.post("/profile", verifyToken, (req,res)=>{
-    jwt.verify(req.token, secretKey, (err, authData)=>{
-        if(err)
-        {
-            res.send({results: "Inavlid Token"});
-        }
-        else
-        {
+// Profile route with Token Verification Middleware
+app.post("/profile", verifyToken, (req, res) => {
+    // Verify the JWT token and respond accordingly
+    jwt.verify(req.token, secretKey, (err, authData) => {
+        if (err) {
+            res.send({ results: "Invalid Token" });
+        } else {
             res.json({
                 message: "Profile accessed",
                 authData,
@@ -55,46 +42,38 @@ app.post("/profile", verifyToken, (req,res)=>{
     });
 });
 
-
-//Token verification middleware
-function verifyToken(req, res, next)
-{
+// Token Verification Middleware
+function verifyToken(req, res, next) {
     const bearerHeader = req.headers["authorization"];
 
-    if(typeof bearerHeader != "undefined") 
-    {
-        //new token generate
+    // Check if the token is present in the request header
+    if (typeof bearerHeader != "undefined") {
+        // Split the header to get the token
         const bearer = bearerHeader.split(" ");
         const token = bearer[1];
 
+        // Set the token in the request for further processing
         req.token = token;
-        next(); //helps generate new token again and again
-    }
-
-    else{
+        next(); // Continue to the next middleware or route
+    } else {
+        // Respond with an error if the token is not present
         res.send({
             result: "Token is not valid",
         });
     }
 }
 
-
-
-
-//server actication
-const startServer = async()=>
-{
-    //synchronous code
-    try{
-        app.listen(PORT, ()=>{
+// Server activation function
+const startServer = async () => {
+    // Synchronous code to start the server and handle errors
+    try {
+        app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT} `);
         });
-
-    }
-    catch(error)
-    {
+    } catch (error) {
         console.log(error);
     }
 }
 
+// Start the server
 startServer();
